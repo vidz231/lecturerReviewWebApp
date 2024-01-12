29072be10e4dcebd30f8c7d50995a8493508344f
Vi Le <viltse180769@fpt.edu.vn>
2024-01-12 15:28:46 +0700

    Refactor registration form component sô that it work with react-hook-form

--- frontend/src/layout/ScreenRegister/RegisterForm.jsx
+++ frontend/src/layout/ScreenRegister/RegisterForm.jsx
@@ -1,80 +1,86 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 
-function RegistrationForm() {
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    // Add your form submission logic here
+// Input field component
+const InputField = ({ type, name, register, errors, label }) => (
+  <div className="relative flex shrink-0 h-20 flex-col mt-2 rounded-3xl border-solid border-black w-full">
+    <header className="relative justify-center text-black text-2xl w-full">
+      {label}
+    </header>
+    <input
+      type={type}
+      name={name}
+      {...register(name, { required: true })}
+      className="w-full h-full border border-solid border-black px-4 rounded-3xl"
+    />
+    {errors[name] && <p className="text-red-500">{`${label} is required`}</p>}
+  </div>
+);
+
+export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
     <form
-      className="w-1/2 mt-3 h-5/6 border flex  flex-col justify-center items-center px-16 py-11 rounded-[74px] border-solid border-black max-md:px-5"
-      onSubmit={handleFormSubmit}
+      onSubmit={handleSubmit(onSubmit)}
+      className="relative h-fit w-2/4 m-auto flex flex-col justify-center items-center border px-16 py-12 rounded-3xl border-solid border-black"
     >
-      <label
-        className="header mt-6 justify-center text-black text-2xl self-stretch max-md:max-w-full"
-        htmlFor="email"
-      >
-        Email:
-      </label>
-      <input
-        className="border self-stretch flex shrink-0 h-[67px] flex-col mt-1.5 rounded-3xl border-solid border-black max-md:max-w-full"
-        type="email"
-        id="email"
-      />
-
-      <label
-        className="header mt-6 justify-center text-black text-2xl self-stretch max-md:max-w-full"
-        htmlFor="username"
-      >
-        Username:
-      </label>
-      <input
-        className="border self-stretch flex shrink-0 h-[67px] flex-col mt-1.5 rounded-3xl border-solid border-black max-md:max-w-full"
+      <InputField
         type="text"
-        id="username"
+        name="name"
+        register={register}
+        errors={errors}
+        label="Name"
       />
-
-      <label
-        className="header mt-6 justify-center text-black text-2xl self-stretch max-md:max-w-full"
-        htmlFor="password"
-      >
-        Password:
-      </label>
-      <input
-        className="border self-stretch flex shrink-0 h-[67px] flex-col mt-1.5 rounded-3xl border-solid border-black max-md:max-w-full"
+      <InputField
+        type="email"
+        name="email"
+        register={register}
+        errors={errors}
+        label="Email"
+      />
+      <InputField
         type="password"
-        id="password"
+        name="password"
+        register={register}
+        errors={errors}
+        label="Password"
       />
-
-      <label
-        className="header mt-6 justify-center text-black text-2xl self-stretch max-md:max-w-full"
-        htmlFor="rePassword"
-      >
-        Re-enter Password:
-      </label>
-      <input
-        className="border self-stretch flex shrink-0 h-[67px] flex-col rounded-3xl border-solid border-black max-md:max-w-full"
+      <InputField
         type="password"
-        id="rePassword"
+        name="confirmPassword"
+        register={register}
+        errors={errors}
+        label="Confirm Password"
       />
 
       <label
-        className="flex gap-2.5 mt-3.5 self-start items-start underline text-blue-500 cursor-pointer "
+        className="flex gap-2.5 mt-3.5 self-start items-start underline text-blue-500 cursor-pointer"
         htmlFor="termsCheckbox"
       >
-        <input type="checkbox" id="termsCheckbox" />
+        <input
+          type="checkbox"
+          id="termsCheckbox"
+          {...register("termsCheckbox", { required: true })}
+        />
         Agree with the Terms of Service
       </label>
+      {errors.termsCheckbox && (
+        <p className="text-red-500">You must agree to the terms of service</p>
+      )}
 
-      <button
-        className="btn-register justify-center text-white text-center text-2xl font-bold bg-lime-600 self-center items-stretch mt-8 px-12 py-4 rounded-[55px] max-md:px-5"
+      <input
         type="submit"
-      >
-        Register
-      </button>
+        className="relative flex-col justify-center text-white text-center text-2xl font-bold bg-lime-600 self-center items-stretch mt-8 px-12 py-4 rounded-[55px] max-md:px-5 cursor-pointer"
+        value="Register"
+      />
     </form>
   );
 }
-
-export default RegistrationForm;
