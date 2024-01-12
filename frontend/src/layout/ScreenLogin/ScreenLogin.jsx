d4e1f5a7d263ec3083c460c3559ff765f14242af
Vi Le <viltse180769@fpt.edu.vn>
2024-01-12 13:49:55 +0700

    Add NavBar component to login and register screens

--- frontend/src/layout/ScreenLogin/ScreenLogin.jsx
+++ frontend/src/layout/ScreenLogin/ScreenLogin.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import LoginForm from "./LoginForm";
+import NavBar from "../../component/NavBar";
 
 export default function ScreenLogin() {
   return (
-    <div className="h-full w-full flex justify-center items-center">
-      <LoginForm />
-    </div>
+    <>
+      <NavBar />
+      <div className="h-full w-full flex justify-center items-center">
+        <LoginForm />
+      </div>
+    </>
   );
 }
--- frontend/src/layout/ScreenRegister/ScreenRegister.jsx
+++ frontend/src/layout/ScreenRegister/ScreenRegister.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import RegisterForm from "./RegisterForm";
+import NavBar from "../../component/NavBar";
 
 export default function ScreenRegister() {
   return (
-    <div className="flex justify-center items-center flex-col w-full h-screen">
-      <h1 className="text-4xl text-center">Register</h1>
-      <RegisterForm />
-    </div>
+    <>
+      <NavBar />
+
+      <div className="flex justify-center items-center flex-col w-full h-screen">
+        <h1 className="text-4xl text-center">Register</h1>
+        <RegisterForm />
+      </div>
+    </>
   );
 }
