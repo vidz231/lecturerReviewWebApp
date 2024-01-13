ddc2d81eb80e7ff2b7da3b6c6bc7786eb63a52b5
Truong <107944687+Pear104@users.noreply.github.com>
2024-01-14 06:30:24 +0700

    init

---
+++ backend/src/main/java/com/learn/reviewgv/ReviewGvApplication.java
@@ -0,0 +1,13 @@
+package com.learn.reviewgv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewGvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewGvApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/learn/reviewgv/ReviewGvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.reviewgv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewGvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
